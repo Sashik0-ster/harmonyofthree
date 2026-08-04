14f4180dbe968fc7a5136ecf68559bd62c70dc6b
Sashik0ster <Sashik0mmm@gmail.com>
2026-08-04 21:15:16 +0200

    change color menu

--- resources/views/components/menu/sidebar-item.blade.php
+++ resources/views/components/menu/sidebar-item.blade.php
@@ -3,7 +3,7 @@
 <a href="{{ $href }}"
     {{ $attributes->merge([
         'class' =>
-            'flex items-center p-1 text-base text-black hover:rounded-t-lg hover:bg-teal-500 group dark:text-gray-500 dark:hover:bg-teal-500 dark:hover:text-white' .
+            'flex items-center p-1 text-base text-black hover:bg-teal-500 hover:rounded-t-lg group dark:text-gray-500 dark:hover:bg-teal-500 dark:hover:text-white' .
             ($active
                 ? 'mt-1 bg-teal-500 text-white rounded-t-lg dark:bg-teal-500 dark:text-white'
                 : 'mt-1 text-black hover:bg-teal-500 dark:text-gray-500 dark:hover:bg-teal-500 dark:hover:text-white'),
